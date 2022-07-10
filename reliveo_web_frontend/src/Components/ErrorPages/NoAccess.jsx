0411c817729afb6603020976fd99d1964c9d7809
Dorian Canonne <d.canonne@gmail.com>
2022-07-10 10:24:42 +0200

    topbar dynamique

--- reliveo_web_frontend/src/Components/ErrorPages/NoAccess.jsx
+++ reliveo_web_frontend/src/Components/ErrorPages/NoAccess.jsx
@@ -1,30 +1,11 @@
 import React from 'react'
-import { useContext } from 'react';
-import * as jose from 'jose';
-import { LogContext } from '../../Context/Contexts';
 
-
-import useGetCookie from '../../Hooks/Get/useGetCookie'
-import { useEffect } from 'react';
-
-function NoAccess({setLoggedUser}) {
+function NoAccess() {
     
-  const [localLoggedUser, setLocalLoggedUser] = useContext(LogContext);
-
-    let cookies = useGetCookie()
-    let jwt = cookies.ReliveoJwt
-
-    useEffect(() => {
-        if(localLoggedUser.username == ""){
-            setLocalLoggedUser(jose.decodeJwt(jwt))
-    
-            console.log(localLoggedUser)
-        }
-    }, []);
-
+  
   return (
     <div>
-    {/* <video src={"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"} autoPlay="true" /> */}
+    <video src={"../../../Assets/Videos/DOG.mp4"} autoPlay="true" />
     </div>
   )
 }
